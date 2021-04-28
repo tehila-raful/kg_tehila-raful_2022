1321ca0a0604a9e26fbebfce992a4ec507b6dc5e
tehila-raful <traful@student.touro.edu>
2021-04-27 22:34:33 -0400

    Initial Commit

---
+++ kargoassessment/Main.java
@@ -0,0 +1,66 @@
+package kargoassessment;
+
+public class Main {
+    public static void main(String[] args) {
+        //create array and display 
+        int[] array = {2, 25, 209};
+        for(int x: array){
+            System.out.print(x + " ");
+        }
+     
+        //create a String[] 
+        String[] numStrings = new String[array.length];
+        String str;
+        StringBuilder strBuilder;
+        
+        //loop through array 
+        for(int j = 0; j < array.length; j++){
+            str = String.valueOf(array[j]);
+            strBuilder = new StringBuilder();
+            
+            //for each character in string convert number
+            //to its string foramt
+            for(int i = 0; i < str.length(); i++){
+                switch(str.charAt(i)){
+                    case '0':
+                        strBuilder.append("Zero");
+                        break;
+                    case '1':
+                        strBuilder.append("One");
+                        break;
+                    case '2':
+                        strBuilder.append("Two");
+                        break;
+                    case '3':
+                        strBuilder.append("Three");
+                        break;    
+                    case '4':
+                        strBuilder.append("Four");
+                        break;
+                    case '5':
+                        strBuilder.append("Five");
+                        break;
+                    case '6':
+                        strBuilder.append("Six");
+                        break;
+                    case '7':
+                        strBuilder.append("Seven");
+                        break;
+                    case '8':
+                        strBuilder.append("Eight");
+                        break;
+                    case '9':
+                        strBuilder.append("Nine");
+                        break;
+                }
+            }
+        //add the numbers as as strings to String array
+        numStrings[j] = strBuilder.toString();
+       
+        }
+        //display numbers once converted 
+        System.out.println();
+        System.out.print(String.join(", ", numStrings));
+    }
+
+}
